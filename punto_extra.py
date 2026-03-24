369cfa7cbaaaa17bd51ada3de877ed9f0fbc3b8d
laragonzalezhub <lari.gonzaleez8@gmail.com>
2026-03-24 00:50:18 -0300

    Probando programa principal y eliminar equipo

--- punto_extra.py
+++ punto_extra.py
@@ -6,7 +6,7 @@ def mostrar_menu():
     print ("3. Mostrar tabla de posiciones")
     print ("4. Salir")
 
-tabla = {}
+
 
 def agregar_equipo (nombre):
     if nombre not in tabla:
@@ -18,32 +18,72 @@ def agregar_equipo (nombre):
     else:
         print ("Este equipo ya esta registrado")
 
-def guardar_resultado (local, visitante, glocal, gvisitante):
+def guardar_resultado (tabla):
+    local = input("Equipo local: ")
+    visitante = input("Equipo visitante: ")
+    glocal = input("Goles local: ")
+    gvisitante = input("Goles visitante: ")
 
     #actualizo partidos 
-    tabla [local][PJ] +=1
-    tabla [visitante][PJ] +=1
+    tabla [local]["PJ"] +=1
+    tabla [visitante]["PJ"] +=1
 
     #guardo goles
-    tabla [local][GF] +=glocal
-    tabla [local][GC] += gvisitante
-    tabla [visitante][GC] += glocal
-    tabla [visitante][GF] +=gvisitante
+    tabla [local]["GF"] +=glocal
+    tabla [local]["GC"] += gvisitante
+    tabla [visitante]["GC"] += glocal
+    tabla [visitante]["GF"] +=gvisitante
 
     #actualizo resultados 
     if glocal > gvisitante:
-        tabla[local][PG] +=1
-        tabla [local][Pts] += 3
-        tabla [visitante][PP]+=1
+        tabla[local]["PG"] +=1
+        tabla [local]["Pts"] += 3
+        tabla [visitante]["PP"]+=1
     elif glocal < gvisitante:
-        tabla [visitante][PG]+=1
-        tabla [visitante][Pts]+= 3
-        tabla [local][PP] +=1
-    else glocal = gvisitante: 
-        tabla [local][Pts]+= 1
-        tabla [visitante][Pts]+= 1
+        tabla [visitante]["PG"]+=1
+        tabla [visitante]["Pts"]+= 3
+        tabla [local]["PP"] +=1
+    else:
+        tabla [local]["Pts"]+= 1
+        tabla [visitante]["Pts"]+= 1
 
-def mostrar_tabla(): 
+def mostrar_tabla(tabla): 
+    print ("TABLA DE POSICIONES")
+    ordenado_por_valor = sorted(tabla.items(), key=lambda item: item[1], reverse =True)
+    for nombre, Pts in ordenado_por_valor:
+        print (f"{nombre}: {Pts}")
+
+def eliminar_equipo (nombre):
+    if nombre in tabla:
+        del tabla[nombre]
+        print (f"equipo eliminado con exito")
+    else:
+        print(f"no existe el equipo")
+
+#Programa principal 
+
+tabla = {}
+while True:
+    mostrar_menu()
+    opcion = input("Elige una opción: ")
+
+    if opcion == "1":
+        agregar_equipo(input("Nombre del equipo"))
+    elif opcion == "2":
+        guardar_resultado(tabla)
+    elif opcion == "3":
+        mostrar_tabla(tabla)
+    elif opcion == "4":
+        eliminar_equipo((input("Nombre del equipo a eliminar")))
+    elif opcion == "5":
+        print("Saliendo del programa...")
+        break
+    else:
+        print("Opción inválida")
+
+
+
+
     
 
 
